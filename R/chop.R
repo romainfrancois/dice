a8f688d1a5a8dd29a1160409f48342cd1133e910
Romain Francois <romain@rstudio.com>
2020-02-03 12:51:16 +0100

    chop() and dice()

--- R/chop.R
+++ R/chop.R
@@ -1,7 +1,16 @@
-#' chop
+#' Chop a vector along the groups of a data frame
 #'
 #' @param data A tibble
-#' @param x A column of `data`
+#' @param x A vector, most often a column of `data`
+#'
+#' @return a list of slices of `x` with one element per group of `data`
+#'
+#' @examples
+#' gdf <- iris %>%
+#'   dplyr::group_by(Species)
+#'
+#' chop(gdf, iris$Sepal.Length)
+#' chop(gdf, 1:150)
 #'
 #' @export
 chop <- function(data, x) {
@@ -29,7 +38,7 @@ chop.rowwise_df <- function(data, x) {
   UseMethod("chop.rowwise_df", x)
 }
 
-#' @method dice.rowwise_df default
+#' @method chop.rowwise_df default
 #' @export
 chop.rowwise_df.default <- function(data, x) {
   map(seq_along(nrow(data)), vec_slice, x = x)
---
+++ R/dice.R
@@ -0,0 +1,45 @@
+
+#' Lazily chop columns of a data frame
+#'
+#' @param data A data frame
+#' @param callback Callback function
+#'
+#' @examples
+#' e <- iris %>%
+#'   dplyr::group_by(Species) %>%
+#'   dice()
+#'
+#' e$Sepal.Length
+#' e$Sepal.Width
+#'
+#' # Using callback ----------------------------------------------------------
+#'
+#' e <- iris %>%
+#'   dplyr::group_by(Species) %>%
+#'   dice(callback = print)
+#'
+#' chunks <- e$Sepal.Width
+#'
+#' @importFrom rlang env chr_unserialise_unicode env_bind_lazy set_names as_function
+#' @importFrom purrr pmap
+#' @export
+dice <- function(data, callback = NULL) {
+  e <- env()
+  names <- chr_unserialise_unicode(names(data))
+
+  promises <- if (is.null(callback)) {
+    map(data, ~expr({
+      chop(!!data, !!.x)
+    }))
+  } else {
+    callback <- as_function(callback)
+    pmap(list(data, names, seq_along(data)), ~expr({
+      res <- chop(!!data, !!..1)
+      callback(list(index = !!..3, name = !!..2, chunks = res))
+      res
+    }))
+  }
+
+  env_bind_lazy(e, !!!set_names(promises, names))
+  e
+}
