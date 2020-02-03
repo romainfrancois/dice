eac0a9fae71a9b6b18b0551b6d5fabd074c1c666
Romain Francois <romain@rstudio.com>
2020-02-03 11:31:36 +0100

    rename dice -> chop

--- R/chop.R
+++ R/chop.R
@@ -1,15 +1,15 @@
-#' Dice
+#' chop
 #'
 #' @param data A tibble
 #' @param x A column of `data`
 #'
 #' @export
-dice <- function(data, x) {
-  UseMethod("dice")
+chop <- function(data, x) {
+  UseMethod("chop")
 }
 
 #' @export
-dice.data.frame <- function(data, x) {
+chop.data.frame <- function(data, x) {
   list(x)
 }
 
@@ -17,32 +17,32 @@ dice.data.frame <- function(data, x) {
 #' @importFrom purrr map
 #' @importFrom vctrs vec_slice
 #' @export
-dice.grouped_df <- function(data, x) {
+chop.grouped_df <- function(data, x) {
   rows <- group_rows(data)
   map(rows, vec_slice, x = x)
 }
 
-#' @rdname dice
-#' @export dice.rowwise_df
-#' @method dice rowwise_df
-dice.rowwise_df <- function(data, x) {
-  UseMethod("dice.rowwise_df", x)
+#' @rdname chop
+#' @export chop.rowwise_df
+#' @method chop rowwise_df
+chop.rowwise_df <- function(data, x) {
+  UseMethod("chop.rowwise_df", x)
 }
 
 #' @method dice.rowwise_df default
 #' @export
-dice.rowwise_df.default <- function(data, x) {
+chop.rowwise_df.default <- function(data, x) {
   map(seq_along(nrow(data)), vec_slice, x = x)
 }
 
-#' @method dice.rowwise_df list
+#' @method chop.rowwise_df list
 #' @export
-dice.rowwise_df.list <- function(data, x) {
+chop.rowwise_df.list <- function(data, x) {
   map(seq_along(nrow(data)), function(i) vec_slice(x, i)[[1L]])
 }
 
-#' @method dice.rowwise_df data.frame
+#' @method chop.rowwise_df data.frame
 #' @export
-dice.rowwise_df.data.frame <- function(data, x) {
+chop.rowwise_df.data.frame <- function(data, x) {
   map(seq_along(nrow(data)), vec_slice, x = x)
 }
